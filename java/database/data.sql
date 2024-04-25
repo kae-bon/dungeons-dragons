7ae8de49285f8bc3de9e1fccd2ebc741458e8679
kae-bon <k.n.bonaguro@gmail.com>
2024-04-25 15:53:35 -0400

    configured starter code to connect to my database, edited schema file to fit my db, and added stock data

--- java/database/data.sql
+++ java/database/data.sql
@@ -3,4 +3,46 @@ BEGIN TRANSACTION;
 INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
 INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
 
+INSERT INTO classes (class_name)
+VALUES ('artificer'),('barbarian'), ('bard'), ('cleric'), ('druid'), ('fighter'), ('monk'),
+	   ('paladin'), ('ranger'), ('rogue'), ('sorcerer'), ('warlock'), ('wizard');
+
+INSERT INTO subclasses (subclass_name, class_id)
+VALUES ('Alchemist', 1), ('Armorer', 1), ('Artillerist', 1), ('Battle Smith', 1),
+('Path of the Ancestral Guardian', 2), ('Path of the Battlerager', 2),
+('Path of the Beast', 2), ('Path of the Berserker', 2), ('Path of the Giant', 2),
+('Path of the Storm Herald', 2), ('Path of the Totem Warrior', 2), ('Path of the Zealot', 2), ('Path of Wild Magic', 2),
+('College of Creation', 3), ('College of Eloquence', 3), ('College of Glamour', 3), ('College of Lore', 3),
+('College of Spirits', 3), ('College of Swords', 3), ('College of Valor', 3), ('College of Whispers', 3),
+('Arcana Domain', 4), ('Death Domain', 4), ('Forge Domain', 4), ('Grave Domain', 4),
+('Knowledge Domain', 4), ('Life Domain', 4), ('Light Domain', 4), ('Nature Domain', 4),
+('Order Domain', 4), ('Peace Domain', 4), ('Tempest Domain', 4), ('Trickery Domain', 4),
+('Twilight Domain', 4), ('War Domain', 4), ('Circle of Dreams', 5), ('Circle of Spores', 5),
+('Circle of Stars', 5), ('Circle of Wildfire', 5), ('Circle of the Land', 5), ('Circle of the Moon', 5),
+('Circle of the Shepherd', 5), ('Arcane Archer', 6), ('Battle Master', 6), ('Cavalier', 6), ('Champion', 6),
+('Echo Knight', 6), ('Eldritch Knight', 6), ('Psi Warrior', 6), ('Purple Dragon Knight', 6),
+('Rune Knight', 6), ('Samurai', 6), ('Way of Mercy', 7), ('Way of Shadow', 7), ('Way of the Ascendant Dragon', 7),
+('Way of the Astral Self', 7), ('Way of the Drunken Master', 7), ('Way of the Four Elements', 7),
+('Way of the Kensei', 7), ('Way of the Long Death', 7), ('Way of the Open Hand', 7),
+('Way of the Sun Soul', 7), ('Oath of Conquest', 8), ('Oath of Devotion', 8), ('Oath of Glory', 8),
+('Oath of Redemption', 8), ('Oath of the Ancients', 8), ('Oath of the Crown', 8), ('Oath of the Watchers', 8),
+('Oath of Vengeance', 8), ('Oathbreaker', 8), ('Beast Master', 9), ('Drakewarden', 9), ('Fey Wanderer', 9),
+('Gloom Stalker', 9), ('Horizon Walker', 9), ('Hunter', 9), ('Monster Slayer', 9), ('Swarmkeeper', 9),
+('Arcane Trickster', 10), ('Assassin', 10), ('Inquisitive', 10), ('Phantom', 10), ('Mastermind', 10), ('Scout', 10), ('Soulknife', 10),
+('Swashbuckler', 10), ('Thief', 10), ('Aberrant Mind', 11), ('Clockwork Soul', 11), ('Divine Soul', 11), ('Draconic Bloodline', 11),
+('Lunar Sorcery', 11), ('Shadow Magic', 11), ('Storm Sorcery', 11), ('Wild Magic', 11),
+('The Archfey', 12), ('The Celestial', 12), ('The Fathomless', 12), ('The Fiend', 12), ('The Genie', 12),
+('The Great Old One', 12), ('The Hexblade', 12), ('The Undead', 12), ('The Undying', 12),
+('Bladesinging', 13), ('Chronurgy Magic', 13), ('Graviturgy Magic', 13), ('Order of Scribes', 13), ('School of Abjuration', 13), ('School of Conjuration', 13),
+('School of Divination', 13), ('School of Enchantment', 13), ('School of Evocation', 13), ('School of Illusion', 13), ('School of Necromancy', 13), ('School of Transmutation', 13), ('War Magic', 13);
+
+INSERT INTO characters (user_id, character_name, character_race, current_level, alignment)
+VALUES (2, 'Neme', 'Satyr', 5, 'Chaotic Neutral'),
+(2, 'Rhywyn', 'Halfling', 5, 'none'),
+(2, 'Chicken', 'Tiefling', 3, 'Chaotic Good');
+
+INSERT INTO character_classes (character_id, class_id, subclass_id)
+VALUES (1, 11, 91), (2, 10, 87), (2, 3, 19),
+(3, 5, 37);
+
 COMMIT TRANSACTION;
--- java/database/schema.sql
+++ java/database/schema.sql
@@ -1,6 +1,6 @@
 BEGIN TRANSACTION;
 
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS users, character_classes, characters, subclasses, classes;
 
 CREATE TABLE users (
 	user_id SERIAL,
@@ -8,6 +8,47 @@ CREATE TABLE users (
 	password_hash varchar(200) NOT NULL,
 	role varchar(50) NOT NULL,
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
+);
+
+CREATE TABLE characters (
+	character_id SERIAL,
+	user_id INTEGER NOT NULL,
+	character_name VARCHAR(100) NOT NULL,
+	character_race VARCHAR(50) NOT NULL,
+	current_level INTEGER NOT NULL,
+	alignment VARCHAR(50) NOT NULL,
+
+	CONSTRAINT PK_character PRIMARY KEY (character_id),
+	CONSTRAINT FK_userid FOREIGN KEY (user_id) REFERENCES users (user_id),
+	CONSTRAINT CK_level CHECK (current_level <= 20 AND current_level > 0)
+);
+
+CREATE TABLE classes (
+	class_id SERIAL,
+	class_name VARCHAR(100) NOT NULL UNIQUE,
+
+	CONSTRAINT PK_class PRIMARY KEY (class_id)
+);
+
+CREATE TABLE subclasses (
+	subclass_id SERIAL,
+	subclass_name VARCHAR(100) NOT NULL,
+	class_id INTEGER NOT NULL,
+
+	CONSTRAINT PK_subclass PRIMARY KEY (subclass_id),
+	CONSTRAINT FK_class_id FOREIGN KEY (class_id) REFERENCES classes (class_id)
+);
+
+CREATE TABLE character_classes (
+	char_class_id SERIAL,
+	character_id INTEGER NOT NULL,
+	class_id INTEGER NOT NULL,
+	subclass_id INTEGER NOT NULL,
+
+	CONSTRAINT PK_char_class PRIMARY KEY (char_class_id),
+	CONSTRAINT FK_class_id FOREIGN KEY (class_id) REFERENCES classes (class_id),
+	CONSTRAINT FK_character_id FOREIGN KEY (character_id) REFERENCES characters (character_id),
+	CONSTRAINT FK_subclass_id FOREIGN KEY (subclass_id) REFERENCES subclasses (subclass_id)
 );
 
 COMMIT TRANSACTION;
